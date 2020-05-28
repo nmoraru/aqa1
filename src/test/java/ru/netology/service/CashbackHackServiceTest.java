ba18489454d8dfd10d27ef398caf31e7db2c4de7
nikita moraru <test@mail.com>
2020-05-28 09:45:20 +0300

    added Junit5 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
+    // JUnit4 tests
     @Test
     public void shoudAmountUnderBoundary() {
         int actual = service.remain(400);
@@ -29,6 +30,32 @@ public class CashbackHackServiceTest {
         int expected = 400;
 
         assertEquals(actual, expected);
+    }
+
+    // JUnit5 tests
+
+    @org.junit.jupiter.api.Test
+    public void shoudAmountUnderBoundaryJ5() {
+        int actual = service.remain(400);
+        int expected = 600;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAmountEqualsBoundaryJ5() {
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAmountAboveBoundaryJ5() {
+        int actual = service.remain(2600);
+        int expected = 400;
+
+        assertEquals(expected, actual);
     }
 
 }
